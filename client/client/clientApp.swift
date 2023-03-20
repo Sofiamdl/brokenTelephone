d82ddf9046f46ca692129cf3d342aefd15f0eaab
Sofiamdl <52216863+Sofiamdl@users.noreply.github.com>
2023-03-20 14:42:53 -0300

    Add files via upload

---
+++ client/client/clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  clientApp.swift
+//  client
+//
+//  Created by sml on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
