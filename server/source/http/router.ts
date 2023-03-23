ab1c2dcddbb4761ea21120e22cbaf7f0af5d7cca
Matheus Batista <matheus.vbatista04@gmail.com>
2023-03-23 01:08:47 -0300

    chore(refactor): add delete user use case router

--- server/source/http/router.ts
+++ server/source/http/router.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { auth } from "./controllers/users/auth-user";
+import { deleteUser } from "./controllers/users/delete-user";
 
 export async function appRoutes(app: FastifyInstance) {
     app.post("/auth", auth);
+    app.delete("/users/:id", deleteUser);
 }
