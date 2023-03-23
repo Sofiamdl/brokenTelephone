a33b2a11225a0e13849d94357b2fe076c61b45c8
Matheus Batista <matheus.vbatista04@gmail.com>
2023-03-23 01:14:05 -0300

    chore(refactor): add get user use case router

--- server/source/http/router.ts
+++ server/source/http/router.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { auth } from "./controllers/users/auth-user";
 import { deleteUser } from "./controllers/users/delete-user";
+import { getUser } from "./controllers/users/get-user";
 
 export async function appRoutes(app: FastifyInstance) {
     app.post("/auth", auth);
     app.delete("/users/:id", deleteUser);
+    app.get("/users/:id", getUser)
 }
