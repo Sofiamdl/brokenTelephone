50fa56a46c903e242709af85151624718e20a2e3
Matheus Batista <matheus.vbatista04@gmail.com>
2023-03-28 17:14:41 -0300

    feature(phrases-repo): add phrases repo interface

---
+++ server/source/repositories/phrases-repository.ts
@@ -0,0 +1,17 @@
+
+export interface IGameObject {
+    userId: string
+    type: string
+    data: string
+}
+
+export interface IThreadObject {
+    [userId: string]: IGameObject[]
+    
+}
+
+export interface IPhrasesRepository {
+    createRoomThreads(users: string[]): Promise<IThreadObject>
+    addGameObjectToThread(userId: string, gameObject: IGameObject): Promise<IThreadObject>
+    findThreadByUserId(userId: string): Promise<IGameObject[]>
+}
