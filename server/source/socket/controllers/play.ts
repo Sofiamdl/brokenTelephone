a132a40bd104978ff7374bd0112500c7d6593f3e
Sofia Melo <sofianovica@hotmail.com>
2023-03-29 15:53:12 -0300

    feature(send-phrase): send phrase to everyone

--- server/source/socket/controllers/play.ts
+++ server/source/socket/controllers/play.ts
@@ -1,17 +1,26 @@
-import { Socket } from "socket.io";
+import { Server, Socket } from "socket.io";
 import { IGamesRepository } from "../../repositories/games-repository";
 import { IPhrasesRepository } from "../../repositories/phrases-repository";
 
-export function play(socket: Socket, phrasesRepository: IPhrasesRepository, gameRepository: IGamesRepository, data: any) {
+export async function play(socket: Socket, phrasesRepository: IPhrasesRepository, gameRepository: IGamesRepository, data: any, io: Server) {
     const roomCode = data;
-    const room = gameRepository.findRoomByCode(roomCode)
+    const room = await gameRepository.findRoomByCode(roomCode)
 
-    room.then(function (val) {
-        val.users.length
-        console.log(val);
-    })
-    const phrases = phrasesRepository.findMany(6)
+    const users = room.users
+
+    const phrases = await phrasesRepository.findMany(users.length)
+
+    for (let i = 0; i< users.length; i++){
+        io.to(users[i].id).emit("phrase", phrases[i])
+    }
     //criar thread
     // pego o vetor de usuarios, pra cada um da um emit com a frase dele
 
 }
+
+// export interface IGameUser {
+//     name: string
+//     id: string
+//     score: number
+//     roomCode: string
+// }
