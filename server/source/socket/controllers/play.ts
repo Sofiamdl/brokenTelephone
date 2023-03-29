49e38084c3792d573918c916b44befddc0038f22
Sofia Melo <sofianovica@hotmail.com>
2023-03-29 16:46:27 -0300

    feature(send-phrases): add play controller

--- server/source/socket/controllers/play.ts
+++ server/source/socket/controllers/play.ts
@@ -1,26 +1,25 @@
 import { Server, Socket } from "socket.io";
 import { IGamesRepository } from "../../repositories/games-repository";
 import { IPhrasesRepository } from "../../repositories/phrases-repository";
+import { IGameObject, IThreadsRepository } from "../../repositories/threads-repository";
 
-export async function play(socket: Socket, phrasesRepository: IPhrasesRepository, gameRepository: IGamesRepository, data: any, io: Server) {
+export async function play(socket: Socket, phrasesRepository: IPhrasesRepository, gameRepository: IGamesRepository, threadRepository: IThreadsRepository, data: any, io: Server) {
     const roomCode = data;
-    const room = await gameRepository.findRoomByCode(roomCode)
 
+    const room = await gameRepository.findRoomByCode(roomCode)
     const users = room.users
+
+    const usersIds = users.map(({id}) => id)
+    const thread = await threadRepository.createRoomThreads(usersIds)
+    console.log(thread)
 
     const phrases = await phrasesRepository.findMany(users.length)
 
     for (let i = 0; i< users.length; i++){
         io.to(users[i].id).emit("phrase", phrases[i])
+        const userGameObject: IGameObject = { data:phrases[i], type: "phrase", userId:users[i].id}
+        await threadRepository.addGameObjectToThread(users[i].id, userGameObject)
     }
-    //criar thread
-    // pego o vetor de usuarios, pra cada um da um emit com a frase dele
 
 }
-
-// export interface IGameUser {
-//     name: string
-//     id: string
-//     score: number
-//     roomCode: string
-// }
+// JOGO var = (indiceUsuarioNoArray + 1) mod (quantidadeUsuarios)
