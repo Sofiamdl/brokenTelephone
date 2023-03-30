acf1584c17597c35c4da2b12f42286cdb250bdb6
Matheus Batista <matheus.vbatista04@gmail.com>
2023-03-30 12:07:55 -0300

    feature(get-drawings): add rounds to game rooms

--- server/source/repositories/in-memory/inmemory-game-repository.ts
+++ server/source/repositories/in-memory/inmemory-game-repository.ts
@@ -7,9 +7,10 @@ export class InMemoryGamesRepository implements IGamesRepository {
         const newGameRoom: IGameRoom = {
             code: gameRoomCode,
             hostId: host.id,
+            round: 0,
             users: [host],
         }
-        
+
         this.gameRooms.push(newGameRoom);
 
         console.log(this.gameRooms);
@@ -44,8 +45,8 @@ export class InMemoryGamesRepository implements IGamesRepository {
     async findRoomByUserId(userId: string) {
         const room = this.gameRooms.find(item => item.users.some(user => user.id === userId));
 
-        if(!room) {
-            return null;
+        if (!room) {
+            throw new Error();
         }
 
         return room;
@@ -54,13 +55,13 @@ export class InMemoryGamesRepository implements IGamesRepository {
     async findUserInRoom(userId: string, gameRoomCode: string) {
         const gameRoom = this.gameRooms.find(item => item.code === gameRoomCode);
 
-        if(!gameRoom) {
+        if (!gameRoom) {
             return null;
         }
 
         const userInRoom = gameRoom.users.find(item => item.id === userId);
 
-        if(!userInRoom) {
+        if (!userInRoom) {
             return null;
         }
 
@@ -70,13 +71,13 @@ export class InMemoryGamesRepository implements IGamesRepository {
     async saveGameUser(user: IGameUser) {
         const gameRoomIndex = this.gameRooms.findIndex(item => item.code === user.roomCode);
 
-        if(gameRoomIndex < 0) {
+        if (gameRoomIndex < 0) {
             throw new Error();
         }
 
         const userIndex = this.gameRooms[gameRoomIndex].users.findIndex(item => item.id === user.id);
 
-        if(userIndex < 0) {
+        if (userIndex < 0) {
             throw new Error();
         }
 
@@ -88,7 +89,7 @@ export class InMemoryGamesRepository implements IGamesRepository {
     async addUserToGameRoom(user: IGameUser, gameRoomCode: string) {
         const gameRoomIndex = this.gameRooms.findIndex(item => item.code === gameRoomCode);
 
-        if(gameRoomIndex < 0) {
+        if (gameRoomIndex < 0) {
             throw new Error();
         }
 
@@ -102,13 +103,13 @@ export class InMemoryGamesRepository implements IGamesRepository {
     async removeUserFromGameRoom(userId: string, gameRoomCode: string) {
         const gameRoomIndex = this.gameRooms.findIndex(item => item.code === gameRoomCode);
 
-        if(gameRoomIndex < 0) {
+        if (gameRoomIndex < 0) {
             throw new Error();
         }
 
         const userIndex = this.gameRooms[gameRoomIndex].users.findIndex(item => item.id === userId);
 
-        if(userIndex < 0) {
+        if (userIndex < 0) {
             throw new Error();
         }
 
@@ -117,6 +118,7 @@ export class InMemoryGamesRepository implements IGamesRepository {
         this.gameRooms[gameRoomIndex] = {
             code: this.gameRooms[gameRoomIndex].code,
             hostId: this.gameRooms[gameRoomIndex].hostId,
+            round: this.gameRooms[gameRoomIndex].round,
             users: newRoomUsers,
         };
 
