ce5ac7d4bb59ac6f89f52711980d15a7da94a6c3
Sofia Melo <sofianovica@hotmail.com>
2023-04-04 17:19:33 -0300

    feature(podium-winners-screen): adjust height

--- client/client/Views/Core/WinnersView/WinnersView.swift
+++ client/client/Views/Core/WinnersView/WinnersView.swift
@@ -24,13 +24,14 @@ struct WinnersView: View {
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(maxWidth: .infinity)
+                        .padding(.top, 80)
                     HStack {
                         WinnersRibbon(winner: "Barreto", image: "cachorro-caramelo", place: .secondPlace, votes: "3")
-                            .padding(.top, 100)
+                            .padding(.top, 80)
                         WinnersRibbon(winner: "Laula", image: "cachorro-caramelo", place: .firstPlace, votes: "3")
                             .padding(.top, -40)
                         WinnersRibbon(winner: "Mavi", image: "cachorro-caramelo", place: .thirdPlace, votes: "3")
-                            .padding(.top, 100)
+                            .padding(.top, 80)
 
                     }
                  
